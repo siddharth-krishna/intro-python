0bfc5342d62cf5bda741d6903401b4a0f0f83831
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-06-09 17:50:28 +0200

    Some refactoring

--- pydojo/dist/index.js
+++ pydojo/dist/index.js
@@ -10,17 +10,22 @@ function builtinRead(x) {
   return Sk.builtinFiles["files"][x];
 }
 
-// Run code using Skulpt
-function runIt() {
-  document.getElementById("output").innerHTML = '';
+function runPy(prog, onSuccess, onFailure) {
   Sk.pre = "output";
   Sk.configure({ output: outf, read: builtinRead, __future__: Sk.python3 });
   var myPromise = Sk.misceval.asyncToPromise(function () {
-    return Sk.importMainWithBody("<stdin>", false, jar.toString(), true);
+    return Sk.importMainWithBody("<stdin>", false, prog, true);
   });
-  myPromise.then(function (mod) {
-    console.log('success');
-  },
+  myPromise.then(onSuccess, onFailure);
+}
+
+// Run code using Skulpt
+function runIt() {
+  document.getElementById("output").innerHTML = '';
+  runPy(jar.toString(),
+    function (mod) {
+      console.log('Skulpt: program executed successfully');
+    },
     function (err) {
       console.log(err.toString());
       var container = document.createElement("span");
@@ -34,19 +39,18 @@ function runIt() {
 function submitIt() {
   var tests = document.getElementById("unittests").value;
   document.getElementById("output").innerHTML = '';
-  Sk.pre = "output";
-  Sk.configure({ output: outf, read: builtinRead, __future__: Sk.python3 });
-  var myPromise = Sk.misceval.asyncToPromise(function () {
-    return Sk.importMainWithBody("<stdin>", false, jar.toString() + '\n' + tests, true);
-  });
-  myPromise.then(function (mod) {
-    var results = document.getElementById("yourresults");
-    results.innerHTML = results.innerHTML + "Congratulations! Problem solved successfully.";
-    results.style.color = "green";
-    console.log('success');
-  },
+  runPy(jar.toString() + '\n' + tests,
+    function (mod) {
+      var results = document.getElementById("yourresults");
+      results.innerHTML = "Congratulations! Problem solved successfully.";
+      results.style.color = "green";
+      console.log('Skulpt: program executed successfully');
+    },
     function (err) {
       console.log(err.toString());
+      var results = document.getElementById("yourresults");
+      results.innerHTML = "Sorry, your code is incorrect. See error below:";
+      results.style.color = "red";
       var container = document.createElement("span");
       container.appendChild(document.createTextNode(err));
       container.style.color = "red";
