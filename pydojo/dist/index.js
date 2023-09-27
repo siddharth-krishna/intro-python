47ae2635bf42ebcb67e0db97c87c92e1c2c64ff0
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-09-27 14:57:18 +0200

    Use local storage to remember solved status

--- pydojo/dist/index.js
+++ pydojo/dist/index.js
@@ -36,7 +36,7 @@ function runIt() {
 }
 
 // Run tests and provide feedback
-function submitIt() {
+function submitIt(id) {
   var tests = document.getElementById("problem-unittests").value;
   document.getElementById("problem-output").innerHTML = '';
   runPy(jar.toString() + '\n' + tests,
@@ -44,6 +44,7 @@ function submitIt() {
       var results = document.getElementById("problem-results");
       results.innerHTML = "Congratulations! Problem solved successfully.";
       results.style.color = "green";
+      setProblemSolved(id, true);
       console.log('Skulpt: program executed successfully');
     },
     function (err) {
@@ -69,29 +70,90 @@ let jar = CodeJar(document.querySelector(".editor"), withLineNumbers(Prism.highl
 jar.updateCode("");
 
 // Database of problems:
-// title, description, defaultcode, unittests
+// id, title, description, defaultcode, unittests
 const problemset = [
-  { title: "Sum from <code>1</code> to <code>n</code>", description: "<p> Write a function <code>sum_to(n)</code> that adds up all the numbers from <code>1</code> up to <code>n</code>. If you call <code>sum_to(10)</code>, for example, it should return <code>55</code>. </p>", defaultcode: "def sum_to(n):\n    # Your code goes here\n\nprint(sum_to(10))  # Expected: 55", unittests: "tests = [(n, sum(range(n+1))) for n in [0,10,15,100,98765]]\nfor n, ans in tests:\n    assert ans == sum_to(n), f'sum_to({n}) should be {ans} but was {sum_to(n)}'\n" },
-  { title: "Sum even numbers", description: "<p> Write a function <code>sum_evens(n)</code> that adds up all the <i>even</i> numbers from <code>1</code> up to <code>n</code>. If you call <code>sum_evens(10)</code>, for example, it should return <code>2+4+6+8+10=30</code>. </p>", defaultcode: "def sum_evens(n):\n    # Your code goes here\n\nprint(sum_evens(10))  # Expected: 30", unittests: "tests = [(n, sum(range(0,n+1,2))) for n in [0,10,15,100,98765]]\nfor n, ans in tests:\n    assert ans == sum_evens(n), f'sum_evens({n}) should be {ans} but was {sum_evens(n)}'\n" },
+  { id: "SumN", title: "Sum from <code>1</code> to <code>n</code>", description: "<p> Write a function <code>sum_to(n)</code> that adds up all the numbers from <code>1</code> up to <code>n</code>. If you call <code>sum_to(10)</code>, for example, it should return <code>55</code>. </p>", defaultcode: "def sum_to(n):\n    # Your code goes here\n\nprint(sum_to(10))  # Expected: 55", unittests: "tests = [(n, sum(range(n+1))) for n in [0,10,15,100,98765]]\nfor n, ans in tests:\n    assert ans == sum_to(n), f'sum_to({n}) should be {ans} but was {sum_to(n)}'\n" },
+  { id: "SumEven", title: "Sum even numbers", description: "<p> Write a function <code>sum_evens(n)</code> that adds up all the <i>even</i> numbers from <code>1</code> up to <code>n</code>. If you call <code>sum_evens(10)</code>, for example, it should return <code>2+4+6+8+10=30</code>. </p>", defaultcode: "def sum_evens(n):\n    # Your code goes here\n\nprint(sum_evens(10))  # Expected: 30", unittests: "tests = [(n, sum(range(0,n+1,2))) for n in [0,10,15,100,98765]]\nfor n, ans in tests:\n    assert ans == sum_evens(n), f'sum_evens({n}) should be {ans} but was {sum_evens(n)}'\n" },
 ]
+
+// Ensure problem IDs are distinct:
+const problemIds = new Set();
+for (const problem of problemset) {
+  if (problemIds.has(problem.id)) {
+    window.alert("ERROR: found duplicate problem ID: " + problem.id);
+  }
+  problemIds.add(problem.id);
+}
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+const usingLocalStorage = storageAvailable("localStorage");
+
+// Progress state: map from problem ID to solved=true/false when no local storage
+const progress = new Map();
+
+/** Returns true if the problem with the given ID is solved */
+function getProblemSolved(id) {
+  if (usingLocalStorage) {
+    return window.localStorage.getItem(id) === "true";
+  } else {
+    return progress.get(id) === true;
+  }
+}
+
+/** Set the problem with the given ID to be solved (true) or unsolved (false)  */
+function setProblemSolved(id, solved) {
+  if (usingLocalStorage) {
+    window.localStorage.setItem(id, solved ? "true" : "false");
+  } else {
+    progress.set(id, solved);
+  }
+}
 
 const problemPage = document.getElementById("problem");
 const problemListPage = document.getElementById("problem-list");
 const problemList = document.getElementById("problem-ul");
 
-// Generate the list of problems from problemset:
-for (const problem of problemset) {
-  let li = document.createElement("li");
-  let link = document.createElement("a");
-  link.innerHTML = problem.title;
-  link.href = "#";
-  link.onclick = function () { showProblem(problem) };
-  li.appendChild(link);
-  li.classList.add(Math.random < 0.5 ? "problem-solved" : "problem-unsolved");
-  problemList.appendChild(li);
-}
-
 function showProblemList() {
+  // Re-generate problem list to update solved status (TODO optimize)
+  problemList.innerHTML = "";
+  for (const problem of problemset) {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    link.innerHTML = problem.title;
+    link.href = "#";
+    link.onclick = function () { showProblem(problem) };
+    li.appendChild(link);
+    li.classList.add(getProblemSolved(problem.id) ? "problem-solved" : "problem-unsolved");
+    problemList.appendChild(li);
+  }
+
+  // Show problem list and hide problem & editor
   problemListPage.style.display = "";
   problemPage.style.display = "none";
 }
@@ -102,6 +164,8 @@ function showProblem(problem) {
   document.getElementById("problem-description").innerHTML = problem.description;
   document.getElementById("problem-unittests").innerHTML = problem.unittests;
   jar.updateCode(problem.defaultcode);
+  // Setup submit button:
+  document.getElementById("submit-button").onclick = function () { submitIt(problem.id) };
   // Clear output and results:
   document.getElementById("problem-output").innerHTML = '';
   document.getElementById("problem-results").innerHTML = '';
@@ -109,5 +173,6 @@ function showProblem(problem) {
   problemListPage.style.display = "none";
   problemPage.style.display = "";
 }
-// TODO add status
-// TODO save progress in local storage
+
+// Show problem list on startup:
+showProblemList();
